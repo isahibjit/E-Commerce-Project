94cae0ee18d9a3b0f1caeff4c2d496c79770edfa
isahibjit <sahibjitsinghramgharia@gmail.com>
2025-04-20 13:24:15 +0530

    added more styling in collections page for mobile devices

--- Frontend/src/Pages/CollectionComponents/SortCollection.jsx
+++ Frontend/src/Pages/CollectionComponents/SortCollection.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 
 const SortCollection = ({handleSortChange}) => {
   return (
-    <div className="flex justify-between pt-7">
-    <div className="flex items-center justify-start ">
-      <h1 className="font-semibold  roboto-regular text-gray-500 text-2xl  ">
-        ALL{" "}
+    <div className="flex justify-between   md:pt-7 ">
+    <div className="flex items-center  ">
+      <h1 className="font-semibold  roboto-regular  text-sm text-gray-500 md:text-2xl   ">
+        ALL
         <span className="text-gray-800 font-semibold"> COLLECTIONS</span>
       </h1>
-      <span className="w-13 h-[2px] bg-gray-900"></span>
+      <span className="md:w-13 w-8 h-[2px] bg-gray-900"></span>
     </div>
-    <div>
+    <div className=" ">
       <select
         onChange={(e) => {
           handleSortChange(e.target.value);
         }}
-        className="border-[2px] text-gray-700 border-gray-300 px-2 py-3 rounded-lg"
+        className="border-[2px] text-gray-700 border-gray-300 px-2 py-3 rounded-lg w-[170px] md:text-md text-sm"
       >
         <option value="default" className="px-24">
           Sort By: Relevant
